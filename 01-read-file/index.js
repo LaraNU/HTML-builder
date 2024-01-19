efb341959bceb91366f68f843814678be0629fda
Larisa Novozhilova <kara4516@gmail.com>
2024-01-19 19:27:37 +0300

    fix: change resolve function to join

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-let result = path.resolve('01-read-file', 'text.txt');
+let result = path.join('01-read-file', 'text.txt');
 reader = fs.createReadStream(result); 
   
 reader.on('data', function (chunk) { 
